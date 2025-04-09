fa7c6d5316e69fd3155804797ce889341a9c5ab6
celina <els.celina.6@gmail.com>
2025-04-09 13:49:47 +0200

    passage en python 3.10

--- Backend/app.py
+++ Backend/app.py
@@ -2,8 +2,11 @@ from flask import *
 import os
 from pathlib import Path
 import datetime
+import requests
 
 import models
+
+RASA_API_URL= 'http://localhost:5005/webhooks/rest/webhook'
 
 directoryBack = os.getcwd()
 directoryFront = str(
@@ -20,7 +23,20 @@ def page_principale():
 def page_connexion():
     return render_template("Create-account.html")
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    user_message=request.json['message']
+    print("User Message:", user_message)
 
+    rasa_response = requests.post(RASA_API_URL, json={'message' : user_message})
+    rasa_response_json = rasa_response.json()
+
+    print("Rasa Resoonse:", rasa_response_json)
+
+    bot_response = rasa_response_json[0]['text'] if rasa_response_json else "Désolé, je n'ai pas compris."
+    return jsonify({'response': bot_response})
+
+    
 # à mettre dans des routes selon le js
 """models.create_new_etudiant(
     22101371, "Lampin", "Vivien", datetime.datetime(2003, 10, 4), "test", "IATIC3")
