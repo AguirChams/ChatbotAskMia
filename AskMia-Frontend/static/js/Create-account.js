fd48d9531bdf604ee52a35983e5b1702106a3b4c
Safa <amamousafa98@gmail.com>
2025-04-13 13:26:13 +0200

    Validation du formulaire

--- AskMia-Frontend/static/js/Create-account.js
+++ AskMia-Frontend/static/js/Create-account.js
@@ -2,6 +2,32 @@ function createAccount(formId, redirectUrl) {
   document.getElementById(formId).addEventListener("submit", async function (e) {
     e.preventDefault();
 
+    const surnameInput = document.getElementById("surname");
+    const nameInput = document.getElementById("name");
+    const classInput = document.getElementById("class");
+    const isAlphabetic = /^[A-Za-z]+$/;
+
+    let value = surnameInput.value.trim();
+
+    if (!isAlphabetic.test(value)) {
+      alert("Le nom doit être alphabétique.");
+      return;
+    }
+
+    value = nameInput.value.trim();
+
+    if (!isAlphabetic.test(value)) {
+      alert("Le prénom doit être alphabétique.");
+      return;
+    }
+
+    value = classInput.value.trim().toLowerCase();
+
+    if (!["iatic 3", "iatic 4", "iatic 5"].includes(value)) {
+      alert("La classe doit être IATIC 3, IATIC 4 ou IATIC 5.");
+      return;
+    }
+
     const student = {
       surname: document.getElementById("surname").value,
       name: document.getElementById("name").value,
