d09628d19485003aa2bb1f1a819b79b08aabc3f8
Safa Amamou <amamousafa98@gmail.com>
2025-04-13 13:36:52 +0200

    Appel api create_account

--- AskMia-Frontend/static/js/Create-account.js
+++ AskMia-Frontend/static/js/Create-account.js
@@ -37,7 +37,7 @@ function createAccount(formId, redirectUrl) {
     };
 
     try {
-      const response = await fetch("/api/create_account", {
+      const response = await fetch("http://localhost:5000/api/create_account", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
